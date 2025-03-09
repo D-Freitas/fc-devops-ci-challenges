e2df3ebc752fdb311df180ff6191f5c2cec93282
Davi Israel de Freitas <davi.israel01@gmail.com>
2025-03-09 20:36:31 -0300

    ci: adapt project for sonnarcloud

--- tests/sum.spec.js
+++ tests/sum.spec.js
@@ -1,7 +1,7 @@
 import assert from 'node:assert/strict'
 import { test } from 'node:test'
 
-import { sum } from './sum.js'
+import { sum } from '../src/sum.js'
 
 test('shoud sum two numbers', () => {
   assert.strictEqual(sum(1, 2), 3)
