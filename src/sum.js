ecc573b1468636614273f80f9c08d1caeca684a2
Davi Israel de Freitas <davi.israel01@gmail.com>
2025-03-11 12:47:54 -0300

    feat: normal code again

--- src/sum.js
+++ src/sum.js
@@ -1,19 +1,3 @@
-sum(1, 2);
-
 export function sum(a, b) {
-  default1();
-  default2();
-  default3();
-  default4();
-  default5();
-  default6();
-  default7();
-
-  // Store function references in an array
-  const defaultFunctions = [default1, default2, default3, default4, default5, default6, default7, default8];
-
-  // Sum up the function return values
-  defaultFunctions.slice(0, 7).reduce((acc, fn) => fn() + acc, 0);
-
   return a + b;
 }
