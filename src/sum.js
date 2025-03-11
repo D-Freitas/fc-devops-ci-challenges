2b64a2fe516c63bbfa279a8979be87d987e02afb
Davi Israel de Freitas <davi.israel01@gmail.com>
2025-03-11 12:04:49 -0300

    ci: remover workflow de CI para o branch develop

--- src/sum.js
+++ src/sum.js
@@ -13,7 +13,7 @@ export function sum(a, b) {
   const defaultFunctions = [default1, default2, default3, default4, default5, default6, default7, default8];
 
   // Sum up the function return values
-  const sumAll = defaultFunctions.slice(0, 7).reduce((acc, fn) => fn() + acc, 0);
+  defaultFunctions.slice(0, 7).reduce((acc, fn) => fn() + acc, 0);
 
   return a + b;
 }
